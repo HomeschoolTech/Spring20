30b85e68891057080c5734706f75c875a66a2bef
kyle <williams.kyle@gmx.com>
2020-02-22 18:10:57 -0600

    Minor fixes to project euler

--- ProjectEuler/euler7.cpp
+++ ProjectEuler/euler7.cpp
@@ -2,21 +2,19 @@
 
 int main(){
 	int prime = 0;		//initialize
-	int num_prime = 0;
+	int num_prime = 1;
 	int check = 0;
-	for(int i = 0; num_prime < 10001; ++i){		//check every number until 10,001 primes are found
+	for(int i = 3; num_prime < 10001; ++i){		//check every number until 10,001 primes are found
 		if(i % 2 == 0)check = 1;		//if divisible by two skip to end
 		else{
 			for(int n = 3; n < i; n+=2){	//divide by all numbers less than i 
 				if(i % n == 0){		
-					if(i == n);
-					else check = 1;
+					if(i!= n) check = 1;
 				}
-				else;
 			}
 		}
 		if(check == 0){			//if check was changed num_prime won't increase and prime won't change
-		    	++num_prime;
+		    ++num_prime;
 			prime = i;
 		}
 		check = 0;	//reset check for next loop
