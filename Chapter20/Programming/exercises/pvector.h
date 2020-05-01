cf15cd98846cdea53d1b69e9415b919eea813eef
seth846 <telumda3rd@gmail.com>
2020-05-01 13:37:26 -0500

    pvector.h

---
+++ Chapter20/Programming/exercises/pvector.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector.h>
+#include "memory"
+using namespace std;
+template <typename T>
+class pvector : public vector<T*>{
+public:
+	~pvector(){
+		cout << "pvector destructor" << endl;
+		for(int i = 0; i < this->size(); ++i){
+			delete (this->elem)[i];
+		}
+	}
+};
